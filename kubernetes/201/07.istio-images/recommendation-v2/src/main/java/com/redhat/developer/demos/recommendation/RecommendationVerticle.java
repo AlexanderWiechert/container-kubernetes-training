98881f96ac7090e21fe23520d87e4c4338526b07
spiddy <dimitris.kapanidis@harbur.io>
2018-06-15 20:43:12 +0200

    update istio

--- kubernetes/201/07.istio-images/recommendation-v2/src/main/java/com/redhat/developer/demos/recommendation/RecommendationVerticle.java
+++ kubernetes/201/07.istio-images/recommendation-v2/src/main/java/com/redhat/developer/demos/recommendation/RecommendationVerticle.java
@@ -11,82 +11,102 @@ import io.vertx.ext.web.RoutingContext;
 
 public class RecommendationVerticle extends AbstractVerticle {
 
-    private static final String RESPONSE_STRING_FORMAT = "recommendation v2 from '%s': %d\n";
+  private static final String RESPONSE_STRING_FORMAT = "recommendation v2 from '%s': %d\n";
 
-    private static final String HOSTNAME = parseContainerIdFromHostname(
-        System.getenv().getOrDefault("HOSTNAME", "unknown")
-    );
+  private static final String HOSTNAME = parseContainerIdFromHostname(
+      System.getenv().getOrDefault("HOSTNAME", "unknown"));
 
-    private static final int LISTEN_ON = Integer.parseInt(
-        System.getenv().getOrDefault("LISTEN_ON", "8080")
-    );
+  private static final int LISTEN_ON = Integer.parseInt(System.getenv().getOrDefault("LISTEN_ON", "8080"));
 
-    static String parseContainerIdFromHostname(String hostname) {
-        return hostname.replaceAll("recommendation-v\\d+-", "");
+  static String parseContainerIdFromHostname(String hostname) {
+    return hostname.replaceAll("recommendation-v\\d+-", "");
+  }
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+  /**
+   * Counter to help us see the lifecycle
+   */
+  private int count = 0;
+
+  /**
+   * Flag for throwing a 503 when enabled
+   */
+  private boolean misbehave = false;
+
+  /**
+   * Flag for going slow
+   */
+  private boolean slow = false;
+
+  @Override
+  public void start() throws Exception {
+    Router router = Router.router(vertx);
+    router.get("/").handler(this::timeout);
+    router.get("/").handler(this::logging);
+    router.get("/").handler(this::getRecommendations);
+    router.get("/slow").handler(this::slow);
+    router.get("/fast").handler(this::fast);
+    router.get("/misbehave").handler(this::misbehave);
+    router.get("/behave").handler(this::behave);
+
+    HealthCheckHandler hc = HealthCheckHandler.create(vertx);
+    hc.register("dummy-health-check", future -> future.complete(Status.OK()));
+    router.get("/health").handler(hc);
+
+    vertx.createHttpServer().requestHandler(router::accept).listen(LISTEN_ON);
+  }
+
+  private void logging(RoutingContext ctx) {
+    logger.info(String.format("recommendation request from %s: %d", HOSTNAME, count));
+    ctx.next();
+  }
+
+  private void timeout(RoutingContext ctx) {
+    if (slow) {
+      ctx.vertx().setTimer(3000, handler -> ctx.next());
+    } else {
+      ctx.next();
     }
+  }
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    /**
-     * Counter to help us see the lifecycle
-     */
-    private int count = 0;
-
-    /**
-     * Flag for throwing a 503 when enabled
-     */
-    private boolean misbehave = false;
-
-    @Override
-    public void start() throws Exception {
-        Router router = Router.router(vertx);
-//        router.get("/").handler(this::timeout);
-        router.get("/").handler(this::logging);
-        router.get("/").handler(this::getRecommendations);
-        router.get("/misbehave").handler(this::misbehave);
-        router.get("/behave").handler(this::behave);
-
-        HealthCheckHandler hc = HealthCheckHandler.create(vertx);
-        hc.register("dummy-health-check", future -> future.complete(Status.OK()));
-        router.get("/health").handler(hc);
-
-        vertx.createHttpServer().requestHandler(router::accept).listen(LISTEN_ON);
+  private void getRecommendations(RoutingContext ctx) {
+    if (misbehave) {
+      count = 0;
+      logger.info(String.format("Misbehaving %d", count));
+      ctx.response().setStatusCode(503).end(String.format("recommendation misbehavior from '%s'\n", HOSTNAME));
+    } else {
+      count++;
+      ctx.response().end(String.format(RESPONSE_STRING_FORMAT, HOSTNAME, count));
     }
+  }
 
-    private void logging(RoutingContext ctx) {
-        logger.info(String.format("recommendation request from %s: %d", HOSTNAME, count));
-        ctx.next();
-    }
+  private void slow(RoutingContext ctx) {
+    this.slow = true;
+    logger.info("'slow' has been set to 'true'");
+    ctx.response().end("Following requests to '/' will be delayed by 2 secs\n");
+  }
 
-    private void timeout(RoutingContext ctx) {
-        ctx.vertx().setTimer(3000, handler -> ctx.next());
-    }
+  private void fast(RoutingContext ctx) {
+    this.slow = false;
+    logger.info("'slow' has been set to 'false'");
+    ctx.response().end("Following requests to '/' will no longer have any delay\n");
+  }
 
-    private void getRecommendations(RoutingContext ctx) {
-        if (misbehave) {
-            count = 0;
-            logger.info(String.format("Misbehaving %d", count));
-            ctx.response().setStatusCode(503).end(String.format("recommendation misbehavior from '%s'\n", HOSTNAME));
-        } else {
-            count++;
-            ctx.response().end(String.format(RESPONSE_STRING_FORMAT, HOSTNAME, count));
-        }
-    }
+  private void misbehave(RoutingContext ctx) {
+    this.misbehave = true;
+    logger.info("'misbehave' has been set to 'true'");
+    ctx.response().end("Following requests to '/' will return a 503\n");
+  }
 
-    private void misbehave(RoutingContext ctx) {
-        this.misbehave = true;
-        logger.info("'misbehave' has been set to 'true'");
-        ctx.response().end("Following requests to '/' will return a 503\n");
-    }
+  private void behave(RoutingContext ctx) {
+    this.misbehave = false;
+    logger.info("'misbehave' has been set to 'false'");
+    ctx.response().end("Following requests to '/' will return a 200\n");
+  }
 
-    private void behave(RoutingContext ctx) {
-        this.misbehave = false;
-        logger.info("'misbehave' has been set to 'false'");
-        ctx.response().end("Following requests to '/' will return a 200\n");
-    }
-
-    public static void main(String[] args) {
-        Vertx.vertx().deployVerticle(new RecommendationVerticle());
-    }
+  public static void main(String[] args) {
+    Vertx.vertx().deployVerticle(new RecommendationVerticle());
+  }
 
 }
