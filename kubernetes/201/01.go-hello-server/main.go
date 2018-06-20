7d24c95c0a0b83248143dfb8266d5a0609b6c48f
spiddy <dimitris.kapanidis@harbur.io>
2018-06-20 17:41:37 +0200

    Added ingress and cluster-issuer

--- kubernetes/201/01.go-hello-server/main.go
+++ kubernetes/201/01.go-hello-server/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		hostname, err := os.Hostname()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(w, "Hello, you've requested: %s (%s)\n", r.URL.Path, hostname)
 	})
 
 	http.ListenAndServe(":9000", nil)
